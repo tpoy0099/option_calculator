27b2c25110e83c1ee6a9203698494637406e5ce0
Tpoy <tpoy@live.cn>
2015-11-05 17:02:22 +0800

    version 0.0.2 修正极端隐含波动率情况下bs定价中的错误
    Signed-off-by: Tpoy <tpoy@live.cn>

--- engine_algorithm/bs_pricing_formula.py
+++ engine_algorithm/bs_pricing_formula.py
@@ -3,14 +3,14 @@ import numpy
 import scipy.stats
 from scipy.optimize import brentq
 
-SQRT  = numpy.sqrt
-LOG_F = numpy.log
-EXP_F = numpy.exp
+SQRT = numpy.sqrt
+LOG  = numpy.log
+EXP  = numpy.exp
 NORM_CDF = scipy.stats.norm.cdf
 NORM_PDF = scipy.stats.norm.pdf
 
+IV_LOWER_BOUND = 1e-8
 ###################################################################
-
 #K : option strike price
 #S : price of the underlying asset
 #r : risk-free interest rate, 0.03 as 3%
@@ -27,9 +27,9 @@ NORM_PDF = scipy.stats.norm.pdf
 
 #Black and Scholes
 def BlackScholes_d1(S, K, r, sigma, T):
-    if sigma == 0:
-        return -scipy.inf
-    return (LOG_F(S/K) + (r + sigma**2 / 2) * T) / (sigma * SQRT(T))
+    if sigma > IV_LOWER_BOUND:
+        return (LOG(S/K) + (r + sigma**2 / 2) * T) / (sigma * SQRT(T))
+    return scipy.inf if S > K else -scipy.inf
 
 def BlackScholes_d2(S, K, r, sigma, T):
     #return (LOG_F(S/K) + (r - sigma**2 / 2) * T) / (sigma * SQRT(T))
@@ -38,11 +38,11 @@ def BlackScholes_d2(S, K, r, sigma, T):
 #C(S,t) = N(d1)S - N(d2)Ke**(-rT)
 def BlackScholes_Call_pricing(S, K, r, sigma, T):
     return (NORM_CDF(BlackScholes_d1(S, K, r, sigma, T)) * S -
-            NORM_CDF(BlackScholes_d2(S, K, r, sigma, T)) * K * EXP_F(-r * T))
+            NORM_CDF(BlackScholes_d2(S, K, r, sigma, T)) * K * EXP(-r * T))
 
 #P(S,t) = N(-d2)Ke**(-rT) - N(-d1)S
 def BlackScholes_Put_pricing(S, K, r, sigma, T):
-    return (NORM_CDF(-BlackScholes_d2(S, K, r, sigma, T)) * K * EXP_F(-r * T) -
+    return (NORM_CDF(-BlackScholes_d2(S, K, r, sigma, T)) * K * EXP(-r * T) -
             NORM_CDF(-BlackScholes_d1(S, K, r, sigma, T)) * S)
 
 def Delta_Call(S, K, r, sigma, T):
@@ -52,43 +52,43 @@ def Delta_Put(S, K, r, sigma, T):
     return NORM_CDF(BlackScholes_d1(S, K, r, sigma, T)) - 1
 
 def Gamma(S, K, r, sigma, T):
-    if sigma == 0:
-        return -scipy.inf
-    return NORM_PDF(BlackScholes_d1(S, K, r, sigma, T)) / (S * sigma * SQRT(T))
+    if sigma > IV_LOWER_BOUND:
+        return NORM_PDF(BlackScholes_d1(S, K, r, sigma, T)) / (S * sigma * SQRT(T))
+    return 0
 
 def Vega(S, K, r, sigma, T):
     return NORM_PDF(BlackScholes_d1(S, K, r, sigma, T)) * S * SQRT(T)
 
 def Theta_Call(S, K, r, sigma, T):
     return (-S * sigma * NORM_PDF(BlackScholes_d1(S, K, r, sigma, T)) / (2 * SQRT(T)) -
-            r * K * EXP_F(-r * T) * NORM_CDF(BlackScholes_d2(S, K, r, sigma, T)))
+            r * K * EXP(-r * T) * NORM_CDF(BlackScholes_d2(S, K, r, sigma, T)))
 
 def Theta_Put(S, K, r, sigma, T):
     return (-S * sigma * NORM_PDF(BlackScholes_d1(S, K, r, sigma, T)) / (2 * SQRT(T)) +
-            r * K * EXP_F(-r * T) * NORM_CDF(-BlackScholes_d2(S, K, r, sigma, T)))
+            r * K * EXP(-r * T) * NORM_CDF(-BlackScholes_d2(S, K, r, sigma, T)))
 
 def Rho_Call(S, K, r, sigma, T):
-    return K * T * EXP_F(-r * T) * NORM_CDF(BlackScholes_d2(S, K, r, sigma, T))
+    return K * T * EXP(-r * T) * NORM_CDF(BlackScholes_d2(S, K, r, sigma, T))
 
 def Rho_Put(S, K, r, sigma, T):
-    return -K * T * EXP_F(-r * T) * NORM_CDF(-BlackScholes_d2(S, K, r, sigma, T))
+    return -K * T * EXP(-r * T) * NORM_CDF(-BlackScholes_d2(S, K, r, sigma, T))
 
 #http://www.codeandfinance.com/finding-implied-vol.html
 #Newton's method
-def implied_volatility_newton(option_market_price, pricing_f, S, K, r, T):
-    MAX_ITERATIONS = 100
-    PRECISION = 1.0e-5
-    sigma = 0.5
-    for i in range(0, MAX_ITERATIONS):
-        price = pricing_f(S, K, r, sigma, T)
-        diff = option_market_price - price
-        #print(i, sigma, diff)
-        if abs(diff) < PRECISION:
-            return sigma
-        #x1 = x0 + f(x0) / f'(x0)
-        sigma = sigma + diff / Vega(S, K, r, sigma, T)
-    # value wasn't found, return best guess so far
-    return sigma
+#def implied_volatility_newton(option_market_price, pricing_f, S, K, r, T):
+#    MAX_ITERATIONS = 100
+#    PRECISION = 1e-5
+#    sigma = 0.5
+#    for i in range(0, MAX_ITERATIONS):
+#        price = pricing_f(S, K, r, sigma, T)
+#        diff = option_market_price - price
+#        #print(i, sigma, diff)
+#        if abs(diff) < PRECISION:
+#            return sigma
+#        #x1 = x0 + f(x0) / f'(x0)
+#        sigma = sigma + diff / Vega(S, K, r, sigma, T)
+#    # value wasn't found, return best guess so far
+#    return sigma
 
 #Calculate the Black-Scholes implied volatility using the Brent method (for reference).
 #Return float, a zero of f between a and b.
@@ -96,10 +96,11 @@ def implied_volatility_newton(option_market_price, pricing_f, S, K, r, T):
 #it means f(a) and f(b) must have different signs
 def implied_volatility_brent(option_market_price, pricing_f, S, K, r, T):
     try:
-        return brentq(lambda sigma : option_market_price - pricing_f(S, K, r, sigma, T),
-                      0, 10)
+        v =  brentq(lambda sigma : option_market_price - pricing_f(S, K, r, sigma, T),
+                    0, 10)
+        return v if v > IV_LOWER_BOUND else IV_LOWER_BOUND
     except:
-        return 0
+        return IV_LOWER_BOUND
 
 def Implied_volatility_Call(S, K, r, T, market_option_price):
     return implied_volatility_brent(market_option_price, BlackScholes_Call_pricing, S, K, r, T)
@@ -108,7 +109,6 @@ def Implied_volatility_Put(S, K, r, T, market_option_price):
     return implied_volatility_brent(market_option_price, BlackScholes_Put_pricing, S, K, r, T)
 
 #############################################################
-
 #comparing with
 #http://www.fintools.com/resources/online-calculators/options-calcs/options-calculator/
 def functional_testing(S0, K0, r0, T0, Call_price0, Put_price0):
@@ -124,7 +124,6 @@ def functional_testing(S0, K0, r0, T0, Call_price0, Put_price0):
         print('Theta for per day: ', Theta_Call(S0, K0, r0, sigma0, T0) / 365)
         print('rho for per 100% rho change: ', Rho_Call(S0, K0, r0, sigma0, T0))
     print()
-
     print('================ put option ================')
     sigma0 = Implied_volatility_Put(S0, K0, r0, T0, Put_price0)
     print('implied_vol: ', sigma0)
@@ -138,26 +137,14 @@ def functional_testing(S0, K0, r0, T0, Call_price0, Put_price0):
         print('rho for per 100% rho change: ', Rho_Put(S0, K0, r0, sigma0, T0))
     print()
 
-def plotTermStructure(S, K, r, sigma, T):
-    import matplotlib.pylab as PLT
-
-    print("Delta by implied_volatility")
-    imp_vol = [v*0.01 for v in range(0, 200, 10)]
-    delta_ls = list()
-    for v in imp_vol:
-        delta_ls.append(Delta_Call(S, K, r, v, T))
-
-    PLT.plot(imp_vol, delta_ls)
-    PLT.show()
-
 if __name__ == '__main__':
-    S0 = 19460
-    K0 = 34000
-    r0 = 0.02
-    T0 = 28 / 365
+    import datetime as DT
+    S0 = 22135
+    K0 = 25000
+    r0 = 0.03
+    T0 = (DT.date(2015,9,23) - DT.date(2015,9,11)).days / 365
 
-    Call_price0 = 15
-    Put_price0 = 15009
+    Call_price0 = 0
+    Put_price0 = 1000
 
-    #functional_testing(S0, K0, r0, T0, Call_price0, Put_price0)
-    plotTermStructure(S0, K0, r0, 0.1, T0)
+    functional_testing(S0, K0, r0, T0, Call_price0, Put_price0)
